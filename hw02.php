afaed1a53c222c8e757b70cccb9ada483c30cc7c
queenqnr <51855685+queenqnr@users.noreply.github.com>
2020-05-08 21:24:21 -0400

    Add files via upload
    I was having some problems getting the if to work

---
+++ hw02.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>HTML Forms</h2>
+
+<form action="/action_page.php">
+  <label for="Ecolor">Enter a Background Color:</label><br>
+OR
+  <li>
+  <select name="Background">Choose a Background Color
+     <option value="background-color:white">White</option>
+     <option value="background-color:Blue">Blue</option>
+     <option value="background-color:Yellow">Yellow</option>
+
+  </select>
+</li>
+  <input type="submit" value="Submit">
+
+</form>
+
+<p>If you click the "Submit" button, the form-data will change the background of the chosen color".</p>
+
+
+
+</body>
+</html>
+<?php
+if(isset($_GET["Background"])){
+
+  echo <body style="background-color:white">;
+if orange {
+   echo <body style="background-color:white">;
+  }
+  if(Ecolor($_Ecolor[body style="background-color:''"]);
+
+
+
+?>
